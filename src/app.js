3cd0605d5128cd4a0328a0213c299c56c1d4bd43
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2024-12-16 19:05:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+
+let adj = ["great", "big"];
+
+let noun = ["jogger", "racoon"];
+
+let domain = "";
+
+function domainName(str) {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        domain += pronoun[i] + adj[x] + noun[y] + ".com\n";
+      }
+    }
+  }
+  return domain;
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(domainName(domain));
 };
