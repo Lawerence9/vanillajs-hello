b138a1619b25b98fc027f3ec6078eef71ab4a395
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2024-12-18 20:13:20 +0000

    Correccion ejercicio

--- src/app.js
+++ src/app.js
@@ -5,21 +5,21 @@ import "./assets/img/4geeks.ico";
 
 
 let pronoun = ["the", "our"];
-let adjetive = ["great", "big"];
+let adjective = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-
-window.onload = function() {
-  let domainType = ".com";
-  let domain = "";
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let x = 0; x < adjetive.length; x++) {
-      for (let y = 0; y < noun.length; y++) {
-        domain += `<li class="list-group-item">
-        ${pronoun[i]}${adjetive[x]}${noun[y]}${domainType}</li>
-        `;
-      }
+let domainType = ".com";
+let domain = "";
+for (let i = 0; i < pronoun.length; i++) {
+  for (let x = 0; x < adjective.length; x++) {
+    for (let y = 0; y < noun.length; y++) {
+      domain += `
+        <li class="list-group-item">
+          ${pronoun[i]}${adjective[x]}${noun[y]}${domainType}
+        </li>
+      `;
     }
   }
-  document.querySelector("#webs").innerHTML = domain;
-};
+}
+
+document.querySelector("#webs").innerHTML = domain;
