d5fee54d4497666011dd8c28694d6b9b3620e45b
Álvaro <82092265+Lawerence9@users.noreply.github.com>
2024-12-18 19:37:48 +0000

    Prettier

--- src/app.js
+++ src/app.js
@@ -1,39 +1,20 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 let pronoun = ["the", "our"];
-
-let adj = ["great", "big"];
-
+let adjetive = ["great", "big"];
 let noun = ["jogger", "racoon"];
 
-//let domain = "";
-
-/*function domainName(str) {
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let x = 0; x < adj.length; x++) {
-      for (let y = 0; y < noun.length; y++) {
-        domain += pronoun[i] + adj[x] + noun[y] + ".com\n";
-      }
-    }
-  }
-  return domain;
-}*/
-
 window.onload = function() {
-  //write your code here
-  //console.log(domainName(domain));
   let domainType = ".com";
   let domain = "";
   for (let i = 0; i < pronoun.length; i++) {
-    for (let x = 0; x < adj.length; x++) {
+    for (let x = 0; x < adjetive.length; x++) {
       for (let y = 0; y < noun.length; y++) {
-        domain += `<li class= "list-group-item">
-        ${pronoun[i]}${adj[x]}${noun[y]}${domainType}</li>
+        domain += `<li class="list-group-item">
+        ${pronoun[i]}${adjetive[x]}${noun[y]}${domainType}</li>
         `;
       }
     }
